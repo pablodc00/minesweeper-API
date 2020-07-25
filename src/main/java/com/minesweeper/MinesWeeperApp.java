d0759aba8f75e5e6ec1016c5821fcb4851f89a5f
Pablo DC <pablodc00@gmail.com>
2020-07-24 22:11:07 -0300

    initial commit

---
+++ src/main/java/com/minesweeper/MinesWeeperApp.java
@@ -0,0 +1,13 @@
+package com.minesweeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinesWeeperApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinesWeeperApp.class, args);
+    }	
+	
+}
