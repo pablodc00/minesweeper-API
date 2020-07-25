85ab9d2a2ee45d39dff27e90896141f8d4bbea7a
Pablo DC <pablodc00@gmail.com>
2020-07-25 18:38:50 -0300

    add model classes

---
+++ src/main/java/com/minesweeper/model/Cell.java
@@ -0,0 +1,43 @@
+package com.minesweeper.model;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Cell {
+
+	protected Cell() {}
+
+	public Cell(int row, int column, boolean hasMine) {
+		this.row = row;
+		this.column = column;
+		this.isRevealed = false;
+		this.hasMine = hasMine;
+		this.flag = Flag.NONE;
+	}
+	
+    public enum Flag {
+        NONE, QUESTION_MARK, RED_FLAG;
+    } 	
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private int row;
+    private int column;
+    private boolean isRevealed;
+    private boolean hasMine;
+    @Enumerated(EnumType.STRING)
+    private Flag flag;    
+
+}
+
---
+++ src/main/java/com/minesweeper/model/Game.java
@@ -0,0 +1,48 @@
+package com.minesweeper.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Game {
+
+	protected Game() {}
+	
+	public Game(int rows, int columns, String username, List<Cell> cells) {
+		this.rows = rows;
+		this.columns = columns;
+		this.userName = username;
+		this.cells = cells;
+		this.sartTime = LocalDateTime.now();
+		this.status = Status.IN_PROGRESS;
+	}
+	
+    public enum Status {
+        IN_PROGRESS, LOST, WON;
+    } 	
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private int rows;
+    private int columns;
+    private String userName;
+    List<Cell> cells;
+    private LocalDateTime sartTime;
+    private LocalDateTime endTime;
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+}
